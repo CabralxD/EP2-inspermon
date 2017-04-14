a29b923388c4787efe38019aa1a8c6527eef08a7
DennisZF <denniszf@al.insper.edu.br>
2017-04-14 11:06:04 -0300

    insperdéx atualizada com os 15 inspermons

--- EP2_INSPERMON.py
+++ EP2_INSPERMON.py
@@ -1,21 +1,36 @@
-#inspermons:
-pythonxu={"ataque":30,"defesa":20,"vida":100} #N:0
-fegamel={"ataque":40,"defesa":50,"vida":70} #N:1
-deusvult={"ataque":50,"defesa":10,"vida":50} #N:2
+pythonxu={"ataque":30,"defesa":20,"vida":100} #N:0   
+fegamel={"ataque":40,"defesa":40,"vida":70} #N:1
+deusvult={"ataque":50,"defesa":20,"vida":80} #N:2         # Os 5 primeiros são insperemons de valores "mais fracos"
+bubbassalto={"ataque":55,"defesa":20,"vida":75} #N:3
+charmandela={"ataque":35,"defesa":45,"vida":70} #N:4
+feche={"ataque":40,"defesa":30,"vida":110} #N:5
+celera={"ataque":50,"defesa":50,"vida":80} #N:6
+alohomora={"ataque":60,"defesa":30,"vida":90} #N:7        # 5 inspermons intermediários
+cartertorta={"ataque":65,"defesa":30,"vida":85} #N:8
+zubate={"ataque":45,"defesa":55,"vida":80} #N:9
+roberstoise={"ataque":50,"defesa":40,"vida":120} #N:10
+karcana={"ataque":60,"defesa":60,"vida":90} #N:11
+agaraga={"ataque":70,"defesa":40,"vida":100} #N:12      # Os mais fortes
+viguvigu={"ataque":75,"defesa":40,"vida":95} #N:13
+fairinhow={"ataque":55,"defesa":65,"vida":90} #N:14
 
-insperdex=[["pythonxu",50,20,100,"N:0"],["fegamel",40,40,70,"N:1"],["deusvult",55,10,50,"N:2"]]
+
+insperdex=[["pythonxu",50,20,100,"N:0"],["fegamel",40,40,70,"N:1"],["deusvult",55,10,50,"N:2"],["bubbassalto",55,20,75,"N:3"],
+["charmandela",35,45,70,"N:4"],["feche",40,30,110,"N:5"],["celera",50,50,80,"N:6"],["alohomora",60,30,90,"N:7"],["catertorta",65,30,85,"N:8"],
+["zubate",45,55,80,"N:9"],["roberstoise",50,40,120,"N:10"],["karcana",60,60,90,"N:11"],["agaraga",70,40,100,"N:12"],["viguvigu",75,40,95,"N:13"],
+["fairinhow",55,65,90,"N:14"]]
 insperdéx=["-"]*20
 XP=0
 
 import time
 import random
 
-command="nada"
+command="NADA"
 
 def batalha(mon,ini,insperdéx,insperdex):
-    print("Um inspermón selvagem apareceu!")
+    print("Um Inspermón selvagem apareceu!")
     time.sleep(1.5)
-    print("seu inspermon: {},ataque:{},defesa:{},vida:{}".format(insperdex[mon][0],insperdex[mon][1],insperdex[mon][2],insperdex[mon][3]))
+    print("Seu Inspermón: {},ataque:{},defesa:{},vida:{}".format(insperdex[mon][0],insperdex[mon][1],insperdex[mon][2],insperdex[mon][3]))
     time.sleep(1)
     print("""inspermon selvagem: {},
 		               ataque: {},
@@ -28,21 +43,21 @@ def batalha(mon,ini,insperdéx,insperdex):
     while vidajog>0 and vidaini>0:
         
         if vidajog>0 and vidaini>0:
-            q=input("oque você quer fazer: 'lutar' ou 'fugir'?")
+            q= str(input("O que você quer fazer: 'lutar' ou 'fugir'?")).upper()
         
-        if q=="lutar":
-            print("você atacou!")
+        if q=="LUTAR":
+            print("Você atacou!")
             w=random.randint(0,19)
         
             if w==10:       #ataque crítico!
-                print("ataque crítico!")
+                print("Ataque C R Í T I C O!!!")
             
                 if ((insperdex[mon][1]*1.5)-(insperdex[ini][2]))<0:
-                    print("o inspermon inimigo se defendeu do ataque!")
+                    print("O Inspermon inimigo conseguiu se defender!")
             
                 else:
                     vidaini=(vidaini)-((insperdex[mon][1]*1.5)-(insperdex[ini][2]))
-                    print("a vida do seu inimigo agora é de {}".format(vidaini))
+                    print("A vida do seu inimigo agora é de {}".format(vidaini))
 
             else:    #ataque normal
                 
@@ -51,37 +66,37 @@ def batalha(mon,ini,insperdéx,insperdex):
                 
                 else:
                 	vidaini=vidaini-((insperdex[mon][1])-(insperdex[ini][2]))
-                	print("a vida do seu inimigo agora é de: {}".format(vidaini))
+                	print("A vida do seu inimigo agora é de: {}".format(vidaini))
 		
         	
             
             if vidaini>0:
             	
-                print ("seu oponente ataca!")
+                print ("Seu oponente ataca!")
                 time.sleep(1)
                 if (insperdex[ini][1]-insperdéx[mon][2])<=0:
-                	print("O seu inspermon se defendeu do ataque inimigo!")
+                	print("O seu Inspermon se defendeu do ataque inimigo!")
                 else:	
                 	vidajog=vidajog-(insperdex[ini][1]-insperdéx[mon][2])
-                	print("a vida de seu inspermon agora é de: {}".format(vidajog))
+                	print("A vida de seu Inspermon agora é de: {}".format(vidajog))
                
 	 
             if vidaini<=0:
-            	print ("você venceu!")
+            	print ("Você venceu!!!")
             	if not(insperdex[ini] in insperdéx):
                 	insperdéx[ini]=(insperdex[ini])
-                	print("parabéns você capturou um novo inspermon")
-            	command="nada"
+                	print("Parabéns!!! Você capturou um novo Inspermon!!!")
+            	command="NADA"
         	
             if vidajog<=0:
-            	print("você perdeu...")
-            	command="dormir"
+            	print("Você perdeu...")
+            	command="DORMIR"
 
         
         
-        if q=="fugir":
-            print("você fugiu!")
-            command="fuga"
+        if q=="FUGIR":
+            print("Você fugiu!")
+            command="FUGA"
             break
     
 	
@@ -99,13 +114,13 @@ for h in range(10):
 
 print ("Olá!")
 time.sleep(2)
-print("Bem vindo à inspermon!")
+print("Bem vindo à Inspermon!")
 time.sleep(2)
 print ("Vamos começar!")
 time.sleep(2)
-print("você vai precisar de um insperdéx...")
+print("Você vai precisar de um Insperdéx...")
 time.sleep(2)
-print("agora é hora de escolher o seu primeiro inspermon para começar a sua aventura!")
+print("Agora, é hora de escolher o seu primeiro Inspermon para começar a sua aventura!")
 time.sleep(4)
 insp= str(input("Escolha um Inspermon: pythonxu, fegamel, deusvult: ")).upper()
 
@@ -122,30 +137,30 @@ if insp=="DEUSVULT":
 	mon=2
 
 time.sleep(1.5)
-print("você escolheu {}!".format(insperdex[mon][0]))
+print("Você escolheu {}!".format(insperdex[mon][0]))
 command="nada"
 
 time.sleep(2.5)
-print("lembresse: quando você vir a opção 'o que você quer fazer?' você pode escolher:")
+print("Lembre-se: quando você vir a opção 'O que você quer fazer?', Você poderá escolher entre:")
 print("------passear (para achar inspermons)")
 print("------dormir (para retornar a sua casa e sair do jogo)")
 print("------insperdéx (para ver os innspermons que você encontrou e venceu)")
 print("------trocar (para substituir o inspermon que você utilizara na próxima batalha)")
 
 
-while command!="dormir":
-	command=input("oque voce quer fazer?: ")
+while command!="DORMIR":
+	command=str(input("O que voce quer fazer?: ")).upper()
 	
 	for h in range (3):
 		print("...")
 		time.sleep(1)
 
-	if command=="passear":
-		ini=random.randint(0,2)
+	if command=="PASSEAR":
+		ini=random.randint(0,14)
 		command=batalha(mon,ini,insperdéx,insperdex)
-		if command=="nada":
+		if command=="LUTAR":
 			XP=XP+1
-			print("seu inspermon ganhou experiência!")
+			print("Seu Inspermon ganhou experiência!")
 		
 		if XP==10:
 			for h in range (3):
@@ -153,21 +168,21 @@ while command!="dormir":
 				time.sleep(1)
 			for i in range(1,3):
 				insperdéx[mon][i]=insperdéx[mon][i]+5
-				print("seu inspermon evoluiu!")
+				print("Seu Inspermon evoluiu!")
 				print(insperdéx[mon])
 				XP=1
 
 
-	if command=="insperdéx":
+	if command=="INSPERDÉX":
 		for h in range(len(insperdéx)):
 			print(insperdéx[h])
 	
-	if command=="trocar":
+	if command=="TROCAR":
 		for h in range(len(insperdéx)):
 			print(insperdéx[h])
-		print("escolha um inspermon de seu insperdéx")
+		print("Escolha um Inspermon de seu Insperdéx")
 		y=mon
-		x=input("diga o número do inspermon você gostaria de utilizar?: ")
+		x=input("Diga o número do Inspermon você gostaria de utilizar: ")
 		mon=x
 		if not (x in insperdéx):
 			mon=y 
@@ -187,4 +202,4 @@ while command!="dormir":
 
 
 
-print("você está em  casa!\n até a próxima!\n")
+print("Você está em  casa!\n Até a próxima!\n")
